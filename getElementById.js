c931fcc9d6525ef959cc666f50cd4a2758d2d6a6
Shivam-makwane <shivammakwane25@gmail.com>
2025-01-07 09:08:30 +0530

    get id file added

---
+++ getElementById.js
@@ -0,0 +1,16 @@
+let ans = document.getElementById("main-heading");
+ans.textContent = "Fruit World";
+ans.style.color = "orange";
+
+let ans1 = document.getElementById('header');
+ans1.style.backgroundColor = "green";
+ans1.style.borderBottom = "3px solid orange";
+
+let ans2 = document.getElementById("basket-heading");
+ans2.style.color = "green";
+
+let ans3 = document.createElement("p");
+ans3.textContent = "Please visit us again";
+
+let ans4 = document.getElementById("thanks");
+ans4.appendChild(ans3);
