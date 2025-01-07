32ba6b4e21c4aba5589f294797e9073fefae3b04
Shivam-makwane <shivammakwane25@gmail.com>
2025-01-07 09:47:25 +0530

    querSelector file added

---
+++ querySelector.js
@@ -0,0 +1,10 @@
+document.querySelector("h2").style.color = "brown";
+let ans = document.querySelector("h1");
+ans.style.paddingLeft = "50%";
+document.querySelector("ul").style.backgroundColor = "grey";
+
+let ans2 = document.querySelectorAll("li");
+ans2[1].style.cssText = "color:white; background-Color:brown";
+ans2[3].style.cssText = "color:white; background-Color:brown";
+ans2[0].style.backgroundColor = "lightgrey";
+ans2[2].style.backgroundColor = "lightgrey";
